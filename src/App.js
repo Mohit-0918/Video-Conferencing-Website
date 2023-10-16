796e2fb52d9fd51fff47372992a8b6d79f9403f6
Mohit-0918 <mohit.sharma.2001rip@outlook.com>
2023-10-16 17:21:43 +0530

    added the Navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './CSS/App.css';
+import './CSS/NavBar.css';
+import { NavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      
+    </div>
+  );
+}
+
+export default App;
